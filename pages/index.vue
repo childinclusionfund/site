39bb0e105ee5a51d128d98d3a6d042c4249cb0ef
Michael Purdy <michaeljosephpurdy@gmail.com>
2020-10-11 23:24:30 -0400

    fix bottom padding

--- pages/index.vue
+++ pages/index.vue
@@ -20,15 +20,13 @@
       ]"
     />
     <Chunk 
+      class="last"
       overview="Our Reasoning"
       v-bind:text="[
         'Many students in our schools are left out of activities and experiences due to financial hardships their families face.',
         'Child Inclusion Fund helps to alleviate some of the difficult decisions families have to make: money toward food or money for a school trip? While this dilemma may be an easy one when prioritizing needs, often times it leaves children left out or stigmatized. Child Inclusion Fund helps by aiding the family to obtain both.',
         'Child Inclusion Fund believes that children deserve an equalized opportunity to enriched and enhanced academic experiences.',
       ]"
-    />
-    <Chunk
-      overview=""
     />
     <Footer />
   </body>
@@ -78,5 +76,8 @@ body {
 }
 a {
   color: var(--text-color);
+}
+.last {
+  padding-bottom: 2rem;
 }
 </style>
