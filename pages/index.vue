1de18950111af467a8574321300ce02b3d8c5b5b
Michael Purdy <michaeljosephpurdy@gmail.com>
2020-11-19 16:03:06 -0500

    Fix typo

--- pages/index.vue
+++ pages/index.vue
@@ -26,7 +26,7 @@
       class="last"
       overview="Our Reasoning"
       v-bind:text="[
-        'Family income has a varied impact on a child’s well-being. While many government and community-ran programs assist with basic necessities, not many are available to fund extra experiences in the school setting. Some families are forced to choose between purchasing school pictures or buying groceries.',
+        'Family income has a varied impact on a child’s well-being. While many government and community-run programs assist with basic necessities, not many are available to fund extra experiences in the school setting. Some families are forced to choose between purchasing school pictures or buying groceries.',
         'Child Inclusion Fund helps alleviate some of the difficult decisions families have to make.',
         'Stigmas associated with low income decreases self-esteem and peer acceptance. Research suggests that interventions during early childhood may be most important in reducing this impact on children. Child Inclusion Fund believes that children will better thrive in the school setting when stigmas are decreased and experiences are enhanced. ',
       ]"
